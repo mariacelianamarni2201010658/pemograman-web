c908048254c1ae1fc701efb1a7af4b48e81eed27
mariacelianamarni2201010658 <127914115+mariacelianamarni2201010658@users.noreply.github.com>
2023-03-29 12:16:43 +0800

    Add files via upload

---
+++ sesi3/latihan05.php
@@ -0,0 +1,12 @@
+<?php
+//model 1
+$age =array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+//print_r($age);
+
+//echo "Umur dari Peter adalah ".$age["Peter"];
+foreach($age as $p => $u ){
+    echo $p . " umurnya " . $u;
+
+    echo "<br>";
+}
